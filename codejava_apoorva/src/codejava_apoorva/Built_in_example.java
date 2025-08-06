94ead9d8ae9740817a4de308952472b13f0a9cd6
chaitra <kjapoorva243@gmail.com>
2025-08-06 20:10:22 +0530

    Fourth commit

---
+++ codejava_apoorva/src/codejava_apoorva/Built_in_example.java
@@ -0,0 +1,20 @@
+package codejava_apoorva;
+
+public class Built_in_example {
+
+	public static void main(String[] args) {
+			String greeting="Hello Apoorva";
+			System.out.println("Original String -"+greeting);
+			System.out.println("Upper case -"+greeting.toUpperCase());
+			System.out.println("substring -"+greeting.substring(6));
+			
+			int max=Math.max(10,20);
+			double sqrt=Math.sqrt(30.0);
+			System.out.println("max number "+ max);
+			System.out.println("sqrt "+sqrt);
+			}
+
+
+	}
+
+
