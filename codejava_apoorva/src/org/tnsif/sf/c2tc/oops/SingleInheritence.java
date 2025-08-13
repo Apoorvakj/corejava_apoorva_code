05649bd1b4d3c133979d23893091c99fee3baee7
chaitra <kjapoorva243@gmail.com>
2025-08-13 18:03:24 +0530

    ninth commit

---
+++ codejava_apoorva/src/org/tnsif/sf/c2tc/oops/SingleInheritence.java
@@ -0,0 +1,35 @@
+package org.tnsif.sf.c2tc.oops;
+
+class Course //parent
+{
+	String coursename="Python Programming";
+	
+	void showCourse()
+	{
+		System.out.println("Course :"+coursename);
+	}
+}
+class Student extends Course //child
+{
+	String name="Apoorva";
+	
+	void showStudent()
+	{
+		System.out.println("Student:" + name);
+	}
+}
+
+public class SingleInheritence {
+
+
+	public static void main(String[] args) {
+		Student s=new Student();
+		s.showStudent();
+		s.showCourse();
+
+	}
+
+}
+
+
+
