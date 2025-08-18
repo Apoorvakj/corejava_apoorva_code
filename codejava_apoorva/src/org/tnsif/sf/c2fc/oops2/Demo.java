28212d53d9e57ac8d1601ef55e78966387f30362
chaitra <kjapoorva243@gmail.com>
2025-08-18 22:45:26 +0530

    tenth commit

---
+++ codejava_apoorva/src/org/tnsif/sf/c2fc/oops2/Demo.java
@@ -0,0 +1,20 @@
+package org.tnsif.sf.c2fc.oops2;
+class Add
+{
+	int a=13;
+	int b=9;
+	void add()
+	{
+		System.out.println(a+b);
+	}
+}
+public class Demo {
+
+	public static void main(String[] args) {
+		Add a1=new Add();
+		a1.add();
+
+
+	}
+
+}
---
+++ codejava_apoorva/src/org/tnsif/sf/c2fc/oops2/Supervariable.java
@@ -0,0 +1,26 @@
+package org.tnsif.sf.c2fc.oops2;
+
+class Notification
+{
+	String message="default Notification";
+}
+class PushNotification extends Notification
+{
+	String message="Push Notification from whatsapp";
+	
+	void showNotification()
+	{
+		System.out.println("child message :"+message);
+		System.out.println("parent message :"+super.message);
+	}
+}
+public class Supervariable {
+
+	public static void main(String[] args) {
+		PushNotification pn=new PushNotification();
+		pn.showNotification();
+
+
+	}
+
+}
